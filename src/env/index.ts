18b8bfeddc3691dc157bc43c796a4151feeb705e
Fred-Reis <fredgost@hotmail.com>
2024-07-19 14:49:07 -0300

    fix: paliative envs

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,11 +4,11 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
-  DATABASE_URL: z.string().optional(),
-  NODEMAILER_PASS: z.string().optional(),
-  NODEMAILER_USER: z.string().optional(),
-  FRONTEND_URL: z.string().optional(),
-  JWT_SECRET: z.string().optional(),
+  DATABASE_URL: z.string(),
+  NODEMAILER_PASS: z.string(),
+  NODEMAILER_USER: z.string(),
+  FRONTEND_URL: z.string(),
+  JWT_SECRET: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
